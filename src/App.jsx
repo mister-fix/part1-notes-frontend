e554ef87905933890bf648ad8ac7f5c211c0f36a
Stephen <hellostephenwm@gmail.com>
2024-05-03 21:47:46 -0400

    Using state for integer counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+
+/*
 const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	const handleClick = () => {
+		console.log("clicked");
+	};
+
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<button onClick={handleClick}>plus</button>
+		</div>
+	);
+};
+*/
+
+/*
+const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<div>
+			<button onClick={() => console.log("clicked")}>plus</button>
+		</div>
+	);
+};
+*/
+
+/*
+const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<div>
+			<div>{counter}</div>
+			<button onClick={() => setCounter(counter + 1)}>plus</button>
+		</div>
+	);
+};
+*/
+
+/*
+const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<div>
+			<div>{counter}</div>
+			<button onClick={() => setCounter(counter + 1)}>plus</button>
+			<button onClick={() => setCounter(0)}>reset</button>
+		</div>
+	);
+};
+*/
+
+const App = () => {
+	const [counter, setCounter] = useState(0);
+	const increaseByOne = () => setCounter(counter + 1);
+	const resetCounter = () => setCounter(0);
+
+	return (
+		<div>
+			<div>{counter}</div>
+			<button onClick={increaseByOne}>plus</button>
+			<button onClick={resetCounter}>reset</button>
 		</div>
 	);
 };
