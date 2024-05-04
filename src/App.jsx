9368c3e888d1d63938ab57b40691193f7e0241d9
Stephen <hellostephenwm@gmail.com>
2024-05-03 21:45:14 -0400

    Using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
+import { useState } from "react";
+
+/*
 const App = () => {
+	const [clicks, setClicks] = useState({
+		left: 0,
+		right: 0,
+	});
+
+	const handleLeftClick = () => {
+		const newClicks = {
+			left: clicks.left + 1,
+			right: clicks.right,
+		};
+
+		setClicks(newClicks);
+	};
+
+	const handleRightClick = () => {
+		const newClicks = {
+			left: clicks.left,
+			right: clicks.right + 1,
+		};
+
+		setClicks(newClicks);
+	};
+
 	return (
 		<div>
-			<h1>Hello World</h1>
+			{clicks.left}
+			<button onClick={handleLeftClick}>left</button>
+			<button onClick={handleRightClick}>right</button>
+			{clicks.right}
 		</div>
 	);
 };
+*/
 
+const App = () => {
+	const [clicks, setClicks] = useState({
+		left: 0,
+		right: 0,
+	});
+
+	const handleLeftClick = () => {
+		setClicks({ ...clicks, left: clicks.left + 1 });
+	};
+
+	const handleRightClick = () => {
+		setClicks({ ...clicks, right: clicks.right + 1 });
+	};
+
+	return (
+		<div>
+			{clicks.left}
+			<button onClick={handleLeftClick}>left</button>
+			<button onClick={handleRightClick}>right</button>
+			{clicks.right}
+		</div>
+	);
+};
 export default App;
